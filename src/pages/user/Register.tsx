fd3348da9590e699865724257a805c0da5a38c44
pranjal23030 <pranjal.khatiwoda@gmail.com>
2025-01-02 21:50:20 +0545

    useDispatcher and useSelector hooks

--- src/pages/user/Register.tsx
+++ src/pages/user/Register.tsx
@@ -1,4 +1,18 @@
+import { useAppDispatch, useAppSelector } from "../../store/hooks"
+import { setAge, setName } from "../../store/userSlice"
+
+
 function Register() {
+
+    // insertion and update garne, POST, PUT/Patch, INSERT, update, --> useDispatch() + types
+    // select garnu paryo, select * from , .find() -- > useSelector() + types
+
+    const data = useAppSelector((store) => store.user)
+    console.log(data)
+    const dispatch = useAppDispatch()
+    dispatch(setName("Tom Jones"))
+    dispatch(setAge(100))
+
     return (
         <h1>Register page </h1>
     )
