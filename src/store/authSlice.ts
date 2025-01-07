240b5ca57c5779dae4ec23175eecf8bf0408ae60
pranjal23030 <pranjal.khatiwoda@gmail.com>
2025-01-07 20:35:42 +0545

    login

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Status } from "../globals/types/type";
-import axios from "axios";
 import { AppDispatch } from "./store";
+import API from "../http";
 
 
 interface ILoginUser {
@@ -46,9 +46,10 @@ export const { setStatus, setUser } = authSlice.actions
 export default authSlice.reducer
 
 export function registerUser(data: IUser) {
+
     return async function registerUserThunk(dispatch: AppDispatch) {
         try {
-            const response = await axios.post("http://localhost:3000/api/auth/register", data)
+            const response = await API.post("/auth/register", data)
             console.log(response)
             if (response.status === 201) {
                 dispatch(setStatus(Status.SUCCESS))
@@ -64,11 +65,10 @@ export function registerUser(data: IUser) {
     }
 }
 
-function loginUser(data: ILoginUser) {
+export function loginUser(data: ILoginUser) {
     return async function loginUserThunk(dispatch: AppDispatch) {
         try {
-            const response = await axios.post("http://localhost:3000/api/auth/login", data)
-            console.log(response)
+            const response = await API.post("/auth/login", data)
             if (response.status === 200) {
                 dispatch(setStatus(Status.SUCCESS))
             } else {
@@ -81,10 +81,11 @@ function loginUser(data: ILoginUser) {
     }
 }
 
-function forgotPassword(data: { email: string }) {
+export function forgotPassword(data: { email: string }) {
     return async function forgotPasswordThunk(dispatch: AppDispatch) {
         try {
-            const response = await axios.post("http://localhost:3000/api/auth/forgot-password", data)
+            const response = await API.post("/auth/forgot-password", data)
+            console.log(response)
             if (response.status === 200) {
                 dispatch(setStatus(Status.SUCCESS))
             } else {
