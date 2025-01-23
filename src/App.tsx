3848b1f78c7860fcebea7f579db66cad8608537c
pranjal23030 <pranjal.khatiwoda@gmail.com>
2025-01-23 21:58:24 +0545

    cart complete

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Home from './pages/home/Home'
 import Login from './pages/user/Login'
 import Product from './pages/product/Product'
 import SingleProduct from './pages/single-product/SingleProduct'
+import MyCart from './pages/cart/my-cart'
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/products' element={<Product />} />
           <Route path='/products/:id' element={<SingleProduct />} />
+          <Route path="/my-cart" element={<MyCart />} />
         </Routes>
       </BrowserRouter>
     </Provider>
--- src/pages/cart/types.ts
+++ src/pages/cart/types.ts
@@ -2,12 +2,25 @@ import { Status } from "../../globals/types/type"
 import { IProduct } from "../product/types"
 
 
+export interface ICartProduct {
+    id: string,
+    productName: string,
+    productImage: string,
+    productPrice: number
+}
 export interface ICartItem {
-    product: IProduct,
-    quantity: number
+    id: string,
+    quantity: number,
+    productId: string,
+    Product: ICartProduct
 }
 
 export interface ICartInitialState {
     items: ICartItem[],
     status: Status
+}
+
+export interface ICartUpdateItem {
+    productId: string,
+    quantity: number
 }
