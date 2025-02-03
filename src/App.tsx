b694066cc54309bb79a0a04f71d7ab76bb3759a6
pranjal23030 <pranjal.khatiwoda@gmail.com>
2025-02-03 19:38:59 +0545

    fetch order

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import Product from './pages/product/Product'
 import SingleProduct from './pages/single-product/SingleProduct'
 import MyCart from './pages/cart/my-cart'
 import Checkout from './pages/checkout/Checkout'
+import MyOrder from './pages/my-orders/MyOrder'
+import MyOrderDetail from './pages/my-orders-details/MyOrderDetail'
 
 function App() {
 
@@ -23,6 +25,8 @@ function App() {
           <Route path='/products/:id' element={<SingleProduct />} />
           <Route path="/my-cart" element={<MyCart />} />
           <Route path="/my-checkout" element={<Checkout />} />
+          <Route path="/my-orders" element={<MyOrder />} />
+          <Route path="/my-orders/:id" element={<MyOrderDetail />} />
         </Routes>
       </BrowserRouter>
     </Provider>
--- src/pages/checkout/types.ts
+++ src/pages/checkout/types.ts
@@ -3,9 +3,15 @@ import { Status } from "../../globals/types/type";
 interface IProduct {
     productId: string,
     productQty: number,
+    orderStatus?: string,
+    totalAmount?: number,
+    Payment?: {
+        paymentMethod: PaymentMethod,
+
+    }
 }
 export interface IOrderItems extends IProduct {
-    orderId: string
+    id: string
 }
 
 export interface IOrder {
