c0aa164af2700bda0ec59537812fc10827bc9fc0
pranjal23030 <pranjal.khatiwoda@gmail.com>
2025-02-15 10:30:14 +0545

    category fetch and delete

--- src/pages/admin/categories/Categories.tsx
+++ src/pages/admin/categories/Categories.tsx
@@ -1,12 +1,25 @@
 import AdminLayout from "../AdminLayout"
 import CategoryTable from "./components/Table"
+import { useEffect, useState } from "react"
+import { API } from "../../../http"
+import { fetchCategoryItems } from "../../../store/adminCategorySlice"
+import { useAppDispatch, useAppSelector } from "../../../store/hooks"
 
 
+export interface ICategory {
+    id: string,
+    categoryName: string
+}
 
 function Categories() {
+    const dispatch = useAppDispatch()
+    const { items: categories } = useAppSelector((store) => store.categories)
+    useEffect(() => {
+        dispatch(fetchCategoryItems())
+    }, [])
     return (
         <AdminLayout>
-            <CategoryTable />
+            <CategoryTable categories={categories} />
 
         </AdminLayout>
     )
